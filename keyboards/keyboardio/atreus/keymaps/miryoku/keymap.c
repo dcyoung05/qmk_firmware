483f3c259c7c6f24a9cc42a6355af579abf92ead
David Young <dcyoung@pobox.com>
2024-08-29 16:41:38 -0500

    Miryoku clone: tweaks, copy/paste keys
    *Add same-hand copy/paste keys to all non-base layers.
    *Set nav layer to default tapping term.
    *Add permissive hold on num layer.

--- keyboards/keyboardio/atreus/keymaps/miryoku/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/miryoku/keymap.c
@@ -20,37 +20,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                      LT(SYM, KC_ENTER), LT(NUM, KC_BSPC), LT(FUN, KC_DEL), KC_NO,   KC_NO,   KC_NO),
 
   [NAV] = LAYOUT( 
-    QK_BOOT, KC_NO,   KC_NO,   KC_NO,  KC_NO,                  LCTL(KC_Y), LCTL(KC_V), LCTL(KC_C), LCTL(KC_X), LCTL(KC_Z),
-    KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,                  KC_CAPS, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,
+    QK_BOOT, LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_NO,      LCTL(KC_Y), LCTL(KC_V), LCTL(KC_C), LCTL(KC_X), LCTL(KC_Z),
+    KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,               KC_CAPS, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_NO, KC_INS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_ENT, KC_BSPC, KC_DEL, KC_NO, KC_NO, KC_NO ),
   
   [MOUSE] = LAYOUT(
-    QK_BOOT, KC_NO,   KC_NO,   KC_NO,  KC_NO,                  LCTL(KC_Y), LCTL(KC_V), LCTL(KC_C), LCTL(KC_X), LCTL(KC_Z),
-    KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,                  KC_NO, MS_LEFT, MS_DOWN, MS_UP, MS_RGHT,
+    QK_BOOT, LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_NO,      LCTL(KC_Y), LCTL(KC_V), LCTL(KC_C), LCTL(KC_X), LCTL(KC_Z),
+    KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,               KC_NO, MS_LEFT, MS_DOWN, MS_UP, MS_RGHT,
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_NO, KC_NO, MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR,
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                MS_BTN2, MS_BTN1, MS_BTN3, KC_NO, KC_NO, KC_NO ),
 
   [MEDIA] = LAYOUT(
-    QK_BOOT, KC_NO,   KC_NO,   KC_NO,  KC_NO,                  LCTL(KC_Y), LCTL(KC_V), LCTL(KC_C), LCTL(KC_X), LCTL(KC_Z),
-    KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,                  KC_NO, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT,
+    QK_BOOT, LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_NO,      LCTL(KC_Y), LCTL(KC_V), LCTL(KC_C), LCTL(KC_X), LCTL(KC_Z),
+    KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,               KC_NO, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT,
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_MSTP, KC_MPLY, KC_MUTE, KC_NO, KC_NO, KC_NO ),
 
   [NUM] = LAYOUT(
-    KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC,                      KC_NO, KC_NO, KC_NO, KC_NO, QK_BOOT,
+    KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC,                      KC_NO, LCTL(KC_V), LCTL(KC_C), LCTL(KC_X), QK_BOOT,
     KC_SCLN, KC_4, KC_5, KC_6, KC_EQUAL,                     KC_NO, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT,
     KC_GRAVE, KC_1, KC_2, KC_3, KC_BSLS, KC_NO,              KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     KC_NO, KC_NO, KC_NO, KC_DOT, KC_0, KC_MINUS,             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO ),
 
   [SYM] = LAYOUT(
-    KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,             KC_NO, KC_NO, KC_NO, KC_NO, QK_BOOT,
+    KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,             KC_NO, LCTL(KC_V), LCTL(KC_C), LCTL(KC_X), QK_BOOT,
     KC_COLN, KC_DLR, KC_PERC, KC_CIRC, KC_PLUS,              KC_NO, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT,
     KC_TILDE, KC_EXLM, KC_AT, KC_HASH, KC_PIPE, KC_NO,       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     KC_NO, KC_NO, KC_NO, KC_LPRN, KC_RPRN, KC_UNDS,          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO ),
 
   [FUN] = LAYOUT(
-    KC_F12, KC_F7, KC_F8, KC_F9, KC_PSCR,                     KC_NO, KC_NO, KC_NO, KC_NO, QK_BOOT,
+    KC_F12, KC_F7, KC_F8, KC_F9, KC_PSCR,                     KC_NO, LCTL(KC_V), LCTL(KC_C), LCTL(KC_X), QK_BOOT,
     KC_F11, KC_F4, KC_F5, KC_F6, KC_SCRL,                     KC_NO, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT,
     KC_F10, KC_F1, KC_F2, KC_F3, KC_PAUSE, KC_NO,             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     KC_NO, KC_NO, KC_NO, KC_ESC, KC_SPC, KC_TAB,             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO ),
@@ -64,8 +64,6 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     case LALT_T(KC_L):
     case LGUI_T(KC_QUOT):
       return 400;
-    case LT(NAV, KC_SPC):
-      return 225;
     default:
       return 200;
   }
@@ -76,6 +74,7 @@ bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
         case LSFT_T(KC_F):
         case LSFT_T(KC_J):
         case LT(NAV, KC_SPC):
+        case LT(NUM, KC_BSPC):
             return true;
         default:
             return false;
